431aaf60b1bd03393c035ffa912e60072721c073
Yuting Li <yuting.li@mail.mcgill.ca>
2025-09-27 16:26:17 -0400

    Refactor main.cpp to implement some test codes with LED control

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 21  // Built-in LED pin for XIAO ESP32S3
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);         // Initialize serial communication
+  pinMode(LED_PIN, OUTPUT);     // Set LED pin as output mode
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  Serial.println("LED is OFF");
+  digitalWrite(LED_PIN, HIGH);  // Turn off LED
+  delay(1000);                  // Wait 1 second
+  
+  Serial.println("LED is ON");
+  digitalWrite(LED_PIN, LOW);   // Turn on LED
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);                  // Wait 1 second
 }
